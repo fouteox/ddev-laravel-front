987a71a9065ed4e3405cc7ea141bb71cd344cfaf
Florian <fouteox@gmail.com>
2025-02-11 19:47:56 +0100

    remove hooks

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -2,11 +2,37 @@
 #ddev-generated
 set -e
 
+if [ "$(uname)" = "Darwin" ]; then
+    sed_inplace() {
+        sed -i '' "$@"
+    }
+else
+    sed_inplace() {
+        sed -i "$@"
+    }
+fi
+
 if [ ! -f package.json ]; then
 		npx -y nuxi@latest init . --gitInit false --packageManager npm --force
 
 		if [ "$HAS_REVERB" = "true" ]; then
         npm install --save laravel-echo pusher-js
+    fi
+
+    CONFIG_FILE="nuxt.config.ts"
+    if [ -f "$CONFIG_FILE" ]; then
+        sed_inplace "s/export default defineNuxtConfig({/export default defineNuxtConfig({\\
+      vite: {\\
+        server: {\\
+          allowedHosts: true\\
+        }\\
+      },\\
+      runtimeConfig: {\\
+        public: {\\
+          clientApiURL: process.env.API_URL_CLIENT,\\
+          serverApiURL: process.env.API_URL_SERVER\\
+        }\\
+      },/" "$CONFIG_FILE"
     fi
 fi
 
