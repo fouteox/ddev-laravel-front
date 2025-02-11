af0076ae8acd54ff9328ea7d4fc09cd320cc101d
Florian <fouteox@gmail.com>
2025-02-11 11:12:29 +0100

    add-on laravel-front

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 if [ ! -f package.json ]; then
 		npx -y nuxi@latest init . --gitInit false --packageManager npm --force
+
+		if [ "$HAS_REVERB" = "true" ]; then
+        npm install --save laravel-echo pusher-js
+    fi
 fi
 
 if [ ! -d "node_modules" ]; then
